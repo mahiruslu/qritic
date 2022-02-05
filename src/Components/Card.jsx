29aa369fc926cfa0b7ff5eb6f96a0119c2050b34
Mahir Mac <mhrsl07@gmail.com>
2022-02-05 18:12:01 +0300

    Project has been initilazed.

---
+++ src/Components/Card.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Card() {
+  return (
+    <div class="p-6 mb-5 w-1/3 mx-auto bg-white rounded-xl shadow-lg flex flex-col items-center space-x-4">
+      <div>
+        <div>
+          <p>This is Title</p>
+        </div>
+        <div>
+          <p>This is count</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Card;
