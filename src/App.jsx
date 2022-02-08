2b91d717c6b7118564a43f26935bf5f85382fd05
Mahir Mac <mhrsl07@gmail.com>
2022-02-08 08:23:18 +0300

    vercel build error

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import Navbar from "./components/Navbar";
+import Navbar from "./components/Navbar.jsx";
 import Explore from "./pages/Explore.jsx";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Users from "./pages/Users";
